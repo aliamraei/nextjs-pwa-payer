a12cc85903e1d51be063139f64b70674395904be
aliamraei <samim.amraei@gmail.com>
2026-04-24 12:05:57 +0330

    update conig

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,15 @@
-// next.config.mjs
-import nextPWA from "next-pwa";
+import NextPwa from "next-pwa";
 
-const withPWA = nextPWA({
+const withPWA = NextPwa({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <--- خروجی استاتیک
   reactStrictMode: true,
   images: {
-    unoptimized: true, // برای export ضروری است
+    unoptimized: true,
   },
 };
 
